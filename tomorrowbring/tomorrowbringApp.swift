c0c552fe04b3b9d2a89b7a00735a672780f6b3b5
Seth Hill <shill@matharts.com>
2026-06-30 14:39:32 -0500

    Initial Commit

---
+++ tomorrowbring/tomorrowbringApp.swift
@@ -0,0 +1,17 @@
+//
+//  tomorrowbringApp.swift
+//  tomorrowbring
+//
+//  Created by Seth Hill on 30.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct tomorrowbringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
